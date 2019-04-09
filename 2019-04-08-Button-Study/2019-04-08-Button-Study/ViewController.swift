302f16eeaddbb4b1e5f89bf7dcf3b2bfaa47e81a
김광준 <kwangjun3952@gmail.com>
2019-04-09 09:37:48 +0900

    xcode: Button Action Study

---
+++ 2019-04-08-Button-Study/2019-04-08-Button-Study/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  2019-04-08-Button-Study
+//
+//  Created by 김광준 on 08/04/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    var temp: Int = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func plusBtn(_ sender: Any) {
+        temp += 1
+        numberLabel.text = String(temp)
+        
+    }
+    
+    
+    @IBAction func minusBtn(_ sender: Any) {
+        temp -= 1
+        numberLabel.text = String(temp)
+    }
+    
+}
+
