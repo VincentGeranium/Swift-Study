dc0e0b86597d27fce4117adb7742803edcb8a4e9
김광준 <kwangjun3952@gmail.com>
2019-04-24 19:20:03 +0900

    앱 델리게이트 클래스의 인스턴스 가져오기, 변수들의 값 저장

--- SubmitValue-Stored/SubmitValue-Stored/SubmitViewController.swift
+++ SubmitValue-Stored/SubmitValue-Stored/SubmitViewController.swift
@@ -40,15 +40,29 @@ class SubmitViewController: UIViewController {
     }
     
     @IBAction func didTabSubmitBtn(_ sender: Any) {
-        let preVC = self.presentingViewController
-        guard let vc = preVC as? ViewController else {
-            return
-        }
+//        let preVC = self.presentingViewController
+//        guard let vc = preVC as? ViewController else {
+//            return
+//        }
+//
+//        vc.paramEmail = self.inputEmail.text
+//        vc.paramUpdata = self.isUpdate.isOn
+//        vc.paramInterval = self.interval.value
         
-        vc.paramEmail = self.inputEmail.text
-        vc.paramUpdata = self.isUpdate.isOn
-        vc.paramInterval = self.interval.value
+        // AppDelegate 객체의 인스턴스를 가져온다.
+        // 붕어빵을 굽는 과정
+        // 타입 캐스팅 (다운 캐스팅)
+        // 이 전체 코드 내에서 가장 주의 깊게 봐야 하는 코드
+        let ad = UIApplication.shared.delegate as? AppDelegate
         
+        // 값을 저장한다
+        // 각각의 변수에 들어오는 값들을 델리게이트에 정의된 변수에 전달
+        ad?.paramEmail = self.inputEmail.text
+        ad?.paramUpdate = self.isUpdate.isOn
+        ad?.paramInterval = self.interval.value
+        
+        // 이전 화면으로 복귀
+        // 나를 띄워주는 뷰 컨트롤러가 있으면 디스미스 하고 아니면 아무것도 하지 않는다
         self.presentingViewController?.dismiss(animated: true, completion: nil)
         
     }
