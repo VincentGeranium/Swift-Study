9ebedb20bc7f23d15826fbd88b271050605dff79
김광준 <kwangjun3952@gmail.com>
2019-04-24 19:18:04 +0900

    델리게이트 객체에 프로퍼티 정의

--- SubmitValue-Stored/SubmitValue-Stored/AppDelegate.swift
+++ SubmitValue-Stored/SubmitValue-Stored/AppDelegate.swift
@@ -12,6 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // 값을 저장할 변수를 정의
+    var paramEmail : String? // 이메일 값을 전달 받을 변수
+    var paramUpdate: Bool? // 자동 갱신 여부를 전달 받을 변수
+    var paramInterval: Double? // 자동 갱신 주기를 전달 받을 변수
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
