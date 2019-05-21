229cd4e542e449fba61dd80d89cba43d90775450
김광준 <kwangjun3952@gmail.com>
2019-05-21 15:23:21 +0900

    xcode: add new edit make imageView on the view use by UICollectionView

---
+++ 2019-05-21-UICollectionView-Image-Study/2019-05-21-UICollectionView-Image-Study/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  2019-05-21-UICollectionView-Image-Study
+//
+//  Created by 김광준 on 21/05/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var images: UIImageView!
+    
+    
+}
