f243a4273450fa850da1c888b2c806c30fcea876
김광준 <kwangjun3952@gmail.com>
2019-06-11 22:07:17 +0900

    fix: delete all of object about on the storyboard and make AutoLayout about Button

--- edwith-Music-App-AutoLayout/edwith-Music-App-AutoLayout/ViewController.swift
+++ edwith-Music-App-AutoLayout/edwith-Music-App-AutoLayout/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.addViewsWithCode()
         self.initializerPlayer()
         
     }
@@ -70,6 +71,43 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         self.timer.invalidate()
         self.timer = nil
     }
+    
+    func addViewsWithCode() {
+        self.addPlayPauseButton()
+    }
+    
+    func addPlayPauseButton() {
+        
+        let button: UIButton = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(button)
+        
+        button.setImage(UIImage(named: "button_play"), for: .normal)
+        button.setImage(UIImage(named: "button_pause"), for: .selected)
+        
+        button.addTarget(self, action: #selector(self.touchUpPlayPauseButton(_:)), for: .touchUpInside)
+        
+        let centerX: NSLayoutConstraint
+        centerX = button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        
+        let centerY: NSLayoutConstraint
+        centerY = NSLayoutConstraint(item: button, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 0.8, constant: 0)
+        
+        let width: NSLayoutConstraint
+        width = button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5)
+        
+        let ratio: NSLayoutConstraint
+        ratio = button.heightAnchor.constraint(equalTo: button.widthAnchor, multiplier: 1)
+        
+        centerX.isActive = true
+        centerY.isActive = true
+        width.isActive = true
+        ratio.isActive = true
+        
+        self.playPauseButton = button
+    }
+    
     
     func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
         guard let error: Error = error else {
