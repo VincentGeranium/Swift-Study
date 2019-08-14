77c5e71c0c0e7e0002e8ed79e6cc9d6a01c8db7c
김광준 <kwangjun3952@gmail.com>
2019-08-15 00:32:52 +0900

    add more example code

--- flatmap.playground/Contents.swift
+++ flatmap.playground/Contents.swift
@@ -5,26 +5,36 @@ let possibleNumber = ["1", "2", "three", "///4///", "5"]
 let mapped: [Int?] = possibleNumber.map { str in Int(str) }
 print("mapped : \(mapped)")
 
-let flatMapped: [Int] = possibleNumber.flatMap {str in Int(str) }
-print("flatMapped : \(flatMapped)")
-
 let compactMapped: [Int] = possibleNumber.compactMap { str in Int(str) }
 print("compactMapped : \(compactMapped)")
+
+let flatMapped: [Int] = possibleNumber.flatMap {str in Int(str) }
+print("flatMapped : \(flatMapped)")
 
 let names: [String?] = ["aa", nil, nil, "dddddd", "eee"]
 let counts = names.compactMap {$0?.count}
 print("counts : \(counts)")
+
+
 
 let scores = [[5,2,7], [4,8], [9,1,3]]
 let allScores = scores.flatMap { $0 }
 
 print("allScores : \(allScores)")
 
+let pass = scores.flatMap { $0.filter { $0 > 5 } }
+print("pass : \(pass)")
+
+
+
+
+
+
 let input: Int? = Int("8")
 print("input: \(String(describing: input))")
 
-let passMark: Int? = input.flatMap {
-    $0 > 5 ? $0 : nil
-}
-
+let passMark: Int? = input.flatMap { $0 > 5 ? $0 : nil }
 print("passMark \(String(describing: passMark))")
+
+
+
