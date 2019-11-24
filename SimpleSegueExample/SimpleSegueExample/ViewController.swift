375ac2d6c2b541dbde2ea025e5e0e4f6faa3b0da
김광준 <kwangjun3952@gmail.com>
2019-11-24 17:54:57 +0900

    edwith ios programming segue example

---
+++ SimpleSegueExample/SimpleSegueExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleSegueExample
+//
+//  Created by 김광준 on 2019/11/24.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
