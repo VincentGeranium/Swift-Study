cb7ae804f1a4bf9aa943d0810e53f23ce9f32a74
김광준 <kwangjun3952@gmail.com>
2019-12-16 23:52:06 +0900

    CustomTableView and CustomCell Example Project

--- 2019-12-16-CreateCustomTableView/2019-12-16-CreateCustomTableView/View/CountryCell.swift
+++ 2019-12-16-CreateCustomTableView/2019-12-16-CreateCustomTableView/View/CountryCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CountryCell: UITableViewCell {
     
+    static let cellId: String = "cellId"
+    
     var countryData: Country? {
         didSet {
             countryNameLabel.text = countryData?.countryName
@@ -36,30 +38,32 @@ class CountryCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        self.contentView.backgroundColor = .cyan
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        let guide = self.contentView.safeAreaLayoutGuide
+        
         addSubview(flagImage)
         addSubview(countryNameLabel)
         
-        self.contentView.backgroundColor = .cyan
-        
-        let guide = self.contentView.safeAreaLayoutGuide
-        
-//        flagImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 5, paddingRight: 0, width: 90, height: 0, enableInsets: false)
-//        countryNameLabel.anchor(top: topAnchor, left: flagImage.rightAnchor, bottom: nil, right: nil, paddingTop: 20, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: frame.size.width / 2, height: 0, enableInsets: false)
-        
         flagImage.translatesAutoresizingMaskIntoConstraints = false
+        
         countryNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             flagImage.topAnchor.constraint(equalTo: guide.topAnchor, constant: 10),
             flagImage.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 10),
             flagImage.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -10),
             flagImage.widthAnchor.constraint(equalToConstant: 90),
             
-//            countryNameLabel.topAnchor.constraint(equalTo: guide.topAnchor, constant: 20),
             countryNameLabel.centerYAnchor.constraint(equalTo: guide.centerYAnchor),
             countryNameLabel.leadingAnchor.constraint(equalTo: flagImage.trailingAnchor, constant: 10),
             countryNameLabel.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -10),
         ])
-        
     }
     
     required init?(coder: NSCoder) {
@@ -68,13 +72,10 @@ class CountryCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
+    
 }
