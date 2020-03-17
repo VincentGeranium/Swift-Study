ee92f3d54c1cdd98efbbb1ec5cf2975b824738fa
김광준 <kwangjun3952@gmail.com>
2020-03-17 16:46:10 +0900

    Enumeration Example Code - 6

---
+++ 2020-03-17-EnumerationExample-6.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+// 특정 항목에 순환 열거형 항목 명시
+//enum ArithmeticExpression {
+//    case number(Int)
+//    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+//    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
+//}
+
+
+// 열거형 전체에 순환 열거형 명시
+indirect enum ArithmeticExpression {
+    case number(Int)
+    case addition(ArithmeticExpression, ArithmeticExpression)
+    case multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+// 순환 열거형의 사용
+let five = ArithmeticExpression.number(5)
+let four = ArithmeticExpression.number(4)
+let sum = ArithmeticExpression.addition(five, four)
+let final = ArithmeticExpression.multiplication(sum, ArithmeticExpression.number(2))
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case .number(let value):
+        return value
+    case .addition(let left, let right):
+        return evaluate(left) + evaluate(right)
+    case .multiplication(let left, let right):
+        return evaluate(left) * evaluate(right)
+    }
+}
+
+let result: Int = evaluate(final)
+print("(5 + 4) * 2 = \(result)")
