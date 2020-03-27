f4b40ee6f7845fd00e64bafb63e0c63938fb92a7
김광준 <kwangjun3952@gmail.com>
2020-03-27 17:01:53 +0900

    Deinitializer Example Code

---
+++ 2020-03-27-DeinitializerExample.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class FileManager {
+    var fileName: String
+    
+    init(fileName: String) {
+        self.fileName = fileName
+    }
+    
+    func openFile() {
+        print("Open File : \(self.fileName)")
+    }
+    
+    func modifyFile() {
+        print("Modify File : \(self.fileName)")
+    }
+    
+    func writeFile() {
+        print("Write File : \(self.fileName)")
+    }
+    
+    func closeFile() {
+        print("File File : \(self.fileName)")
+    }
+    
+    deinit {
+        print("Deinit instance")
+        self.writeFile()
+        self.closeFile()
+    }
+}
+
+var fileManager: FileManager? = FileManager(fileName: "First File.txt")
+
+if let manager: FileManager = fileManager {
+    manager.openFile()
+    manager.modifyFile()
+}
+
+fileManager = nil
