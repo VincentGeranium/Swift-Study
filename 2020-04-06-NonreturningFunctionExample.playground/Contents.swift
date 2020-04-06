c56355027666b7623a67759ec5d15c55b9e02f8f
김광준 <kwangjun3952@gmail.com>
2020-04-06 22:42:22 +0900

    Nonreturning Function And Nonreturning Method Example Code

---
+++ 2020-04-06-NonreturningFunctionExample.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func crashAndBurn() -> Never {
+    fatalError("Something very, very bad happended")
+}
+
+//crashAndBurn()
+
+func someFunction(isAllIsWell: Bool) {
+    guard isAllIsWell else {
+        print("마을에 도둑이 들었습니다!😳")
+        crashAndBurn()
+    }
+    print("All is well 👍")
+}
+
+someFunction(isAllIsWell: true)
+
+someFunction(isAllIsWell: false)
