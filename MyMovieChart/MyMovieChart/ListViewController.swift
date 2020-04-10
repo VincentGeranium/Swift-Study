803d4ca83fc09e60a3cf5bf3a80fd940f640dd7f
김광준 <kwangjun3952@gmail.com>
2020-04-10 23:37:08 +0900

    200410 added tableViewController, set prototype cell

---
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewController.swift
+//  MyMovieChart
+//
+//  Created by 김광준 on 2020/04/10.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+}
