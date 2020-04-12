1d4729a3d3b7134ebad30601e1e7201b4a7dfb78
김광준 <kwangjun3952@gmail.com>
2020-04-12 20:16:46 +0900

    200412 : 데이터 소스 만들기(MovieVO 클래스 작성, ListVC 작성)

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -10,5 +10,34 @@ import Foundation
 import UIKit
 
 class ListViewController: UITableViewController {
+    var list: [MovieVO] = [MovieVO]()
     
+    override func viewDidLoad() {
+        // 첫번째 행
+        var mvo = MovieVO()
+        mvo.title = "다크나이트"
+        mvo.description = "히어로물에 철학에 음악까지 더해져 예술이 되다."
+        mvo.opendate = "2008-09-04"
+        mvo.rating = 8.95
+        // 배열에 추가
+        self.list.append(mvo)
+        
+        // 두번째 행
+        mvo = MovieVO()
+        mvo.title = "호우시절"
+        mvo.description = "때를 알고 내리는 좋은 비"
+        mvo.opendate = "2009-10-08"
+        mvo.rating = 7.31
+        // 배열에 추가
+        self.list.append(mvo)
+        
+        // 세번째 행
+        mvo = MovieVO()
+        mvo.title = "말할 수 없는 비밀"
+        mvo.description = "여기서 너에게까지 다섯 걸음."
+        mvo.opendate = "2015-05-07"
+        mvo.rating = 9.19
+        // 배열에 추가
+        self.list.append(mvo)
+    }
 }
---
+++ MyMovieChart/MyMovieChart/MovieVO.swift
@@ -0,0 +1,20 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by 김광준 on 2020/04/12.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail: String? // 영화 섬네일 이미지 주소
+    var title: String? // 영화 제목
+    var description: String? // 영화 설명
+    var detail: String? // 상세정보
+    var opendate: String? // 개봉일
+    var rating: Double? // 평정
+}
+
+
