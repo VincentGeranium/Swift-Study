9c3ea6808698f0d00bc9a422f35dd76cd64ea243
김광준 <kwangjun3952@gmail.com>
2020-04-16 22:17:45 +0900

    data binding of tableViewCell

--- MyMovieChart/MyMovieChart/ListViewController.swift
+++ MyMovieChart/MyMovieChart/ListViewController.swift
@@ -33,5 +33,29 @@ class ListViewController: UITableViewController {
     }()
     
     override func viewDidLoad() {
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+
+        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as? UITableViewCell else {
+//            return UITableViewCell()
+//        }
+        cell.textLabel?.text = row.title
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
     }
 }
