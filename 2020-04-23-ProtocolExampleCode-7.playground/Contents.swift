9a2313fa072402cbf6847393ff72c249f15e61c7
김광준 <kwangjun3952@gmail.com>
2020-04-23 00:26:34 +0900

    200423 Protocol Summary Example Code

---
+++ 2020-04-23-ProtocolExampleCode-7.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+protocol Named {
+    var name: String { get }
+    
+    init?(name: String)
+}
+
+struct Animal: Named {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+struct Pet: Named {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Person: Named {
+    var name: String
+    
+    required init(name: String) {
+        self.name = name
+    }
+}
+
+class School: Named {
+    var name: String
+    
+    required init?(name: String) {
+        self.name = name
+    }
+}
+
+var someName: Named = Animal(name: "Animal")
+someName = Pet(name: "Pet")
+someName = Person(name: "Person")
+someName = School(name: "School")!
+
