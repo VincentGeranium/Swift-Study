5e0b173c00971a3b8729865feba29bd355f26bb4
김광준 <kwangjun3952@gmail.com>
2020-05-07 01:46:17 +0900

    extension example code

---
+++ 2020-05-06-ExtensionExampleCode-1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+extension Int {
+    var isEven: Bool {
+        return self % 2 == 0
+    }
+    
+    var isOdd: Bool {
+        return self % 2 == 1
+    }
+}
+
+print(1.isEven) // false
+print(2.isEven) // true
+print(1.isOdd) // true
+print(2.isOdd) // false
+
+var number: Int = 3
+print(number.isEven) // false
+print(number.isOdd) // true
+
+number = 2
+print(number.isEven) // true
+print(number.isOdd) // false
