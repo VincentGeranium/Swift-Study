6638b00d6348f4beb10f732e92d309e3576fe547
김광준 <kwangjun3952@gmail.com>
2020-05-14 18:53:16 +0900

    Two-Phase Initialization Example Code

---
+++ 2020-05-14-TwoPhaseInitializationExampleCode-1.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class Person {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+class Student: Person {
+    var major: String
+    
+    init(name: String, age: Int, major: String) {
+        self.major = "Swift"
+        super.init(name: name, age: age)
+    }
+    
+    convenience init(name: String) {
+        self.init(name: name, age: 7, major: "")
+    }
+}
+
