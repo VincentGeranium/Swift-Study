6cfd96ab05cf725cbdb2bd700e756bb1ebf5c20f
김광준 <kwangjun3952@gmail.com>
2020-05-18 21:38:18 +0900

    Type Casting Example Code

---
+++ 2020-05-18-TypeCastingExampleCode-2.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class Coffee {
+    let name: String
+    let shot: Int
+    
+    var description: String {
+        return "\(shot) shot(s) \(name)"
+    }
+    
+    init(shot: Int) {
+        self.shot = shot
+        self.name = "Coffee"
+    }
+}
+
+class Latte: Coffee {
+    var flavor: String
+    
+    override var description: String {
+        return "\(shot) shot(s) \(flavor) latte"
+    }
+    
+    init(flavor: String, shot: Int) {
+        self.flavor = flavor
+        super.init(shot: shot)
+    }
+}
+
+class Americano: Coffee {
+    let iced: Bool
+    
+    override var description: String {
+        return "\(shot) shot(s) \(iced ? "iced" : "hot") americano"
+    }
+    
+    init(shot: Int, iced: Bool) {
+        self.iced = iced
+        super.init(shot: shot)
+    }
+}
+
